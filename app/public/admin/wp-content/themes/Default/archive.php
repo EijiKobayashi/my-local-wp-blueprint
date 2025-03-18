6eaae293f7c3721cd9c5d193a93852761044b3fd
KOBAYASHI Eiji <e@oshinco.com>
2025-03-18 11:47:51 +0900

    archive.php にデフォルトのカテゴリ, タグ処理を付与

--- app/public/admin/wp-content/themes/Default/archive.php
+++ app/public/admin/wp-content/themes/Default/archive.php
@@ -6,6 +6,16 @@ if (!$post_type_name) {
 $post_type_label = esc_html(get_post_type_object(get_post_type())->label);
 $template = dirname(__FILE__) . '/templates/archive/' . $post_type_name . '.php';
 
+// カテゴリー
+if (is_category()) {
+  $template = dirname(__FILE__) . '/templates/archive/category.php';
+}
+
+// タグ
+if (is_tag()) {
+  $template = dirname(__FILE__) . '/templates/archive/tag.php';
+}
+
 if (file_exists($template)) {
   require_once $template;
 } else {
